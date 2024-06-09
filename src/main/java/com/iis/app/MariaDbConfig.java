839f3b0fa736c60823461434edeb6a326e9a6e52
ErickHBorda <212121@unamba.edu.pe>
2024-06-09 12:13:09 -0500

    Conexion a MariaDB, SQL, Postgre, Oracle

--- src/main/java/com/iis/app/MariaDbConfig.java
+++ src/main/java/com/iis/app/MariaDbConfig.java
@@ -1,15 +1,15 @@
 package com.iis.app;
 
-//import java.sql.SQLException;
+import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.mariadb.jdbc.MariaDbPoolDataSource;
+//import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 //import org.postgresql.ds.PGSimpleDataSource;
-//import org.mariadb.jdbc.MariaDbPoolDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+//ORACLE
 //import com.zaxxer.hikari.HikariConfig;
 //import com.zaxxer.hikari.HikariDataSource;
 
@@ -18,7 +18,7 @@ public class MariaDbConfig {
     @Bean
     DataSource dataSource(){
         //MARIA DB
-        /*MariaDbPoolDataSource dataSource = new MariaDbPoolDataSource();
+        MariaDbPoolDataSource dataSource = new MariaDbPoolDataSource();
 
         try {
             dataSource.setUrl("jdbc:mariadb://localhost:3306/dbgeneral");
@@ -28,35 +28,43 @@ public class MariaDbConfig {
             e.printStackTrace();
         }
 
-        return dataSource;*/
-
-        //SQL SERVER
-        SQLServerDataSource dataSource = new SQLServerDataSource();
-        dataSource.setURL("jdbc:sqlserver://localhost:1433;encrypt=true;trustServerCertificate=true;databaseName=dbgeneral");
-        dataSource.setUser("sa");
-        dataSource.setPassword("111");
         return dataSource;
 
+        //SQL SERVER
+        /*SQLServerDataSource dataSource = new SQLServerDataSource();
+        try {
+            dataSource.setURL("jdbc:sqlserver://localhost:1433;encrypt=true;trustServerCertificate=true;databaseName=dbgeneral");
+            dataSource.setUser("sa");
+            dataSource.setPassword("111");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dataSource;*/
+
         //POSTGRESQL
-        /*HikariConfig config = new HikariConfig();
-
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/dbgeneral");
-        config.setUsername("postgres");
-        config.setPassword("111");
-        config.setDriverClassName("org.postgresql.Driver");
-
-        return new HikariDataSource(config);
-        
+        /*PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        try {
+            dataSource.setUrl("jdbc:postgresql://localhost:5432/dbgeneral");
+            dataSource.setUser("postgres");
+            dataSource.setPassword("111");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dataSource;*/
 
         //ORACLE
         /*HikariConfig config = new HikariConfig();
-
-        config.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE"); // Cambia según tu configuración de Oracle
-        config.setUsername("system");
-        config.setPassword("111");
-        config.setDriverClassName("oracle.jdbc.OracleDriver");
-
+        try {
+            config.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+            config.setUsername("system");
+            config.setPassword("111");
+            config.setDriverClassName("oracle.jdbc.OracleDriver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
         return new HikariDataSource(config); */
+        
     } 
     
 }
